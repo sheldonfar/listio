4b22faebdac5b01e73bfbbd550e549c8963f4f81
Yevhenii Minin <sheldonfar@gmail.com>
2023-02-26 16:24:24 +0100

    more linting issues

--- scripts/gh-pages-deploy.mjs
+++ scripts/gh-pages-deploy.mjs
@@ -3,23 +3,23 @@ import { execa } from 'execa'
 
 (async () => {
   try {
-    await execa("git", ["checkout", "--orphan", "gh-pages"]);
+    await execa('git', ['checkout', '--orphan', 'gh-pages'])
     // eslint-disable-next-line no-console
-    console.log("Building started...");
-    await execa("yarn", ["run", "build"]);
+    console.log('Building started...')
+    await execa('yarn', ['run', 'build'])
     // Understand if it's dist or build folder
-    const folderName = fs.existsSync("dist") ? "dist" : "build";
-    await execa("git", ["--work-tree", folderName, "add", "--all"]);
-    await execa("git", ["--work-tree", folderName, "commit", "-m", "gh-pages"]);
-    console.log("Pushing to gh-pages...");
-    await execa("git", ["push", "origin", "HEAD:gh-pages", "--force"]);
-    fs.rmSync(folderName, {recursive: true});
-    await execa("git", ["checkout", "-f", "main"]);
-    await execa("git", ["branch", "-D", "gh-pages"]);
-    console.log("Successfully deployed, check your settings");
+    const folderName = fs.existsSync('dist') ? 'dist' : 'build'
+    await execa('git', ['--work-tree', folderName, 'add', '--all'])
+    await execa('git', ['--work-tree', folderName, 'commit', '-m', 'gh-pages'])
+    console.log('Pushing to gh-pages...')
+    await execa('git', ['push', 'origin', 'HEAD:gh-pages', '--force'])
+    fs.rmSync(folderName, { recursive: true })
+    await execa('git', ['checkout', '-f', 'main'])
+    await execa('git', ['branch', '-D', 'gh-pages'])
+    console.log('Successfully deployed, check your settings')
   } catch (e) {
     // eslint-disable-next-line no-console
-    console.log(e.message);
-    process.exit(1);
+    console.log(e.message)
+    process.exit(1)
   }
-})();
+})()
